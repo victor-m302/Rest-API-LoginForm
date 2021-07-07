2cc02707a521ddb16f767e94381634ebf8e143a5
Victor M <49913795+victor-m302@users.noreply.github.com>
2021-07-07 14:52:05 -0300

    Register complete
    - register working
    - print vector of users
    - redirect to register if error
    - login and register with ejs

--- server.js
+++ server.js
@@ -1,10 +1,47 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt') //npm i bcrypt
+const { response } = require('express')
+const users = []
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false}))
 
 app.get('/', (req, res) => {
     res.render('index.ejs', {name: 'Joe'})
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs', {name: 'Login'})
+})
+
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs', {name: 'Register'})
+})
+
+app.post('/login', (req, res) => {
+    req.body.name
+})
+
+
+app.post('/register', async (req, res) => {
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name, //acessa o name
+            email: req.body.email, //acessa o name
+            password: hashedPassword//req.body.password //acessa o name
+        })
+        res.redirect('/login')
+    }
+    catch{
+        res.redirect('/register')
+    }
+    console.log(users);
+
+
 })
 
 app.listen(3000)
